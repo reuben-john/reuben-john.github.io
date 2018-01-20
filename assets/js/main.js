646a3067f0d8a785c0c30569c2a51d1b20458882
Reuben <rjkoppy@gmail.com>
2018-01-19 22:07:32 -0500

    Add scroll check to change background of navbar when scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,3 +56,22 @@
     offset: 54
   });
 })(jQuery); // End of use strict
+
+/**
+ * Listen to scroll to change header opacity class
+ */
+function checkScroll() {
+  var startY = $(".navbar").height() * 2; //The point where the navbar changes in px
+
+  if ($(window).scrollTop() > startY) {
+    $(".navbar").addClass("scrolled");
+  } else {
+    $(".navbar").removeClass("scrolled");
+  }
+}
+
+if ($(".navbar").length > 0) {
+  $(window).on("scroll load resize", function() {
+    checkScroll();
+  });
+}
