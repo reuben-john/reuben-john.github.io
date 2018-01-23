5ccbe0678e48b469d9b347b4bd82211ce82f2de6
Reuben John <rjkoppy@gmail.com>
2018-01-23 09:54:53 -0500

    Set up eslint using standard.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,76 +1,76 @@
-(function($) {
-  "use strict";
+/* eslint-env jquery, browser */
+
+(function ($) {
+  'use strict'
   // From CSS Tricks
   // Select all links with hashes
   $('a[href*="#"]')
     // Remove links that don't actually link to anything
     .not('[href="#"]')
     .not('[href="#0"]')
-    .click(function(event) {
+    .click(function (event) {
       // On-page links
       if (
-        location.pathname.replace(/^\//, "") ==
-          this.pathname.replace(/^\//, "") &&
+        location.pathname.replace(/^\//, '') ==
+        this.pathname.replace(/^\//, '') &&
         location.hostname == this.hostname
       ) {
         // Figure out element to scroll to
-        var target = $(this.hash);
+        var target = $(this.hash)
         target = target.length
           ? target
-          : $("[name=" + this.hash.slice(1) + "]");
+          : $('[name=' + this.hash.slice(1) + ']')
         // Does a scroll target exist?
         if (target.length) {
           // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $("html, body").animate(
-            {
-              scrollTop: target.offset().top
-            },
-            800,
-            function() {
-              // Callback after animation
-              // Must change focus!
-              var $target = $(target);
-              $target.focus();
-              if ($target.is(":focus")) {
-                // Checking if the target was focused
-                return false;
-              } else {
-                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-              }
+          event.preventDefault()
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          },
+          800,
+          function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target)
+            $target.focus()
+            if ($target.is(':focus')) {
+              // Checking if the target was focused
+              return false
+            } else {
+              $target.attr('tabindex', '-1') // Adding tabindex for elements not focusable
+              $target.focus() // Set focus again
             }
-          );
+          }
+          )
         }
       }
-    });
+    })
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $(".scroller").click(function() {
-    $(".navbar-collapse").collapse("hide");
-  });
+  $('.scroller').click(function () {
+    $('.navbar-collapse').collapse('hide')
+  })
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $("body").scrollspy({
-    target: "#mainNav",
+  $('body').scrollspy({
+    target: '#mainNav',
     offset: 54
-  });
-})(jQuery); // End of use strict
-
+  })
+})(jQuery) // End of use strict
 
 //  Listen to scroll to change header opacity class
-function checkScroll() {
-  var startY = $(".navbar").height() * 2; //The point where the navbar changes in px
+function checkScroll () {
+  var startY = $('.navbar').height() * 2 // The point where the navbar changes in px
 
   if ($(window).scrollTop() > startY) {
-    $(".navbar").addClass("scrolled");
+    $('.navbar').addClass('scrolled')
   } else {
-    $(".navbar").removeClass("scrolled");
+    $('.navbar').removeClass('scrolled')
   }
 }
 
-if ($(".navbar").length > 0) {
-  $(window).on("scroll load resize", function() {
-    checkScroll();
-  });
+if ($('.navbar').length > 0) {
+  $(window).on('scroll load resize', function () {
+    checkScroll()
+  })
 }
